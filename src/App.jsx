acfaef1b1f2bd4bd9ef458ad0b0c3dfb85aac780
Jayanthoutnondgenie <jayanth@outbondgenie.com>
2024-07-27 02:41:35 +0530

    project setup with React Tailwind @ React Testing Library

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline flex justify-center  text-green-600 mt-20">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
